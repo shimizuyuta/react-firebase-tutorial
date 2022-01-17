543410c54ba15929cf74b8efcba143183b958d26
clean_water <shmizuyuta213@icloud.com>
2022-01-18 00:58:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+// import {BrowerRouter as Router,Routes,Route,Link} from 'react-router-dom'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
+import { auth } from "./firebase-config";
+import { signOut } from "firebase/auth";
 
 function App() {
+  const [isAuth,setIsAuth] = useState(localStorage.getItem('isAuth'));
+  const signUserOut = () =>{
+    signOut(auth).then(()=>{
+      localStorage.clear()
+      setIsAuth(false);
+      // navigate("/login")
+      window.location.pathname = "/login";
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        {!isAuth?(
+        <Link to="/login">login</Link>
+        ):(
+        <>
+        <Link to="/createpost">create post</Link>
+        <button onClick={signUserOut}>log out</button>
+        </>
+        )}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home isAuth={isAuth}/>}/>
+        <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
+        <Route path="/createpost" element={<CreatePost isAuth={isAuth}/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,62 @@
+import { collection, getDocs,doc, deleteDoc } from 'firebase/firestore'
+import React,{useEffect,useState} from 'react'
+import { db ,auth} from '../firebase-config'
+
+const Home = ({isAuth}) => {
+  const [postList, setPostList] = useState([])
+  // const postsCollectionRef = collection(db,'posts')
+  // const postsCollectionRef = collection(db, "posts");
+  const postsCollectionRef = collection(db,'posts')
+  console.log(isAuth,'dddddddd')
+
+
+ const deletePost = async(id) =>{
+   const postDoc = doc(db,'posts',id)
+   await deleteDoc(postDoc)
+   getPosts()
+ }
+ const getPosts = async()=>{
+  const data = await getDocs(postsCollectionRef)
+  setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+};
+
+ useEffect(() => {
+
+
+  getPosts()
+},[])
+
+  return (
+    <div className="homePage">
+    {postList.map((post,index) => {
+      return (
+        <div className="post" key={index}>
+          <div className="postHeader">
+            <div className="title">
+              <h1> {post.title}</h1>
+            </div>
+            <div className="deletePost">
+              {isAuth && post.author.id === auth.currentUser.uid && (
+                <button
+                  onClick={() => {
+                    // deletePost(post.id);]
+                    deletePost(post.id)
+                  }}
+                >
+                  {" "}
+                  &#128465;
+                </button>
+              )}
+            </div>
+          </div>
+
+          <div className="postTextContainer"> {post.postText} </div>
+          <h3>@{post.author.name}</h3>
+        </div>
+      );
+    })}
+  </div>
+  )
+}
+
+export default Home
