1d9c6b5d1605a9d74f0e2ab895ef40d5049f6bfb
clean_water <shmizuyuta213@icloud.com>
2022-01-20 04:22:15 +0900

    google認証機能切り分け

---
+++ src/component/LoginWithGoogle.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import {getAuth,GoogleAuthProvider,signInWithPopup} from 'firebase/auth';
+
+
+const LoginWithGoogle = ({ setIsAuth }) => {
+  let navigate = useNavigate();
+  const auth = getAuth();
+
+  const clickLoginWithGoogle = () =>{
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth,provider)
+    .then((result)=>{
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log('user info',user)
+      setIsAuth(true)
+      navigate('/')
+
+    }).catch((error)=>{
+      const errorCode = error.code;
+      console.log(errorCode)
+    })
+  }
+
+  return (
+  <div className="loginPage">
+    <p>Sign In With Google to Continue</p>
+    <button className="login-with-google-btn" onClick={clickLoginWithGoogle}>
+      Sign in with Google
+    </button>
+  </div>
+  )
+}
+
+export default LoginWithGoogle
